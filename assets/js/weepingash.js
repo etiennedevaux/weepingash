9fbd3762a7d0080229395f766d37627e3436fced
Stephen Clarke <60775095+etiennedevaux@users.noreply.github.com>
2021-01-01 16:10:24 +0000

    First commit
    Primary purpose to establish connectivity

---
+++ assets/js/weepingash.js
@@ -0,0 +1,13 @@
+// Global Variables
+var jsfileversion="0002";
+function showMenuComponent(sect) {
+var elements = document.getElementsByClassName("MenuComponent");
+
+    for (var i = 0; i < elements.length; i++){
+        elements[i].style.display = "none";
+    }
+var x = document.getElementById(sect);
+x.style.display = "block";
+document.getElementById("JSSerNo").textContent="."+jsfileversion;
+}
+
